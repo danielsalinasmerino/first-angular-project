f4146618213b095b39e518aa9da1151533724b87
danielsalinasmerino <danielsalinas305@gmail.com>
2026-03-14 13:11:26 +0100

    feat(user): require avatar and name inputs in user component
    * Updated @Input decorators for avatar and name to require values.
    * Ensures that user component receives necessary data for rendering.

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -7,8 +7,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./user.css'],
 })
 export class User {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
