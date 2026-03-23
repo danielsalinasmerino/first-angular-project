72df665166d3580a3ade243717b705467d3adabb
Daniel <danielsalinas305@gmail.com>
2026-03-23 14:17:18 +0100

    feat(tasks): implement task rendering and user task filtering
    * Update task component to use dynamic data binding for task properties.
    * Refactor tasks component to filter tasks based on selected user.
    * Add dummy tasks data for testing purposes.
    * Introduce TaskType interface for better type safety.

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TaskType } from '../../types/task';
 
 @Component({
   selector: 'app-task',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './task.html',
   styleUrl: './task.css',
 })
-export class Task {}
+export class Task {
+  @Input({ required: true }) task!: TaskType;
+}
--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Task } from './task/task';
+import { DUMMY_TASKS } from '../dummy-tasks';
+import { UserType } from '../types/user';
 
 @Component({
   selector: 'app-tasks',
@@ -8,5 +10,10 @@ import { Task } from './task/task';
   styleUrl: './tasks.css',
 })
 export class Tasks {
-  @Input() name?: string;
+  @Input() user?: UserType;
+  allTasks = DUMMY_TASKS;
+
+  get selectedUserTasks() {
+    return this.allTasks.filter((task) => task.userId === this.user?.id);
+  }
 }
