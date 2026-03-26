da59fe260b543419466dcbe6bd7349552978177d
Daniel <danielsalinas305@gmail.com>
2026-03-26 14:04:26 +0100

    feat(tasks): add new task component and functionality
    * Create new-task component with HTML and CSS files.
    * Integrate new-task component into tasks template.
    * Implement task addition functionality in tasks component.

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -2,15 +2,19 @@ import { Component, Input } from '@angular/core';
 import { Task } from './task/task';
 import { DUMMY_TASKS } from '../dummy-tasks';
 import { UserType } from '../user/user.model';
+import { NewTask } from './new-task/new-task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task, NewTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
 export class Tasks {
   @Input() user?: UserType;
+
+  isAddingTask = false;
+
   tasks = DUMMY_TASKS;
 
   get selectedUserTasks() {
@@ -19,5 +23,9 @@ export class Tasks {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
